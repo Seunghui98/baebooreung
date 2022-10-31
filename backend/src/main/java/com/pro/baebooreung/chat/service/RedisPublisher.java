4b6c98a1729953601df566a9fc9af5746389da6f
daseul <dew4325@naver.com>
2022-10-31 14:40:57 +0900

    [B]ADD_Swagger

--- backend/src/main/java/com/pro/baebooreung/chat/service/RedisPublisher.java
+++ backend/src/main/java/com/pro/baebooreung/chat/service/RedisPublisher.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+/**
+ * 레디스 발행 서비스 구현
+ * 채팅방에 입장하여 메시지를 작성하면 해당 메시지를 Redis Topic에 발행하는 서비스
+ * 이 서비스를 통해 메시지를 발행하면 대기하고 있던 redis 구독 서비스가 메시지를 처리
+ */
 public class RedisPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
--- backend/src/main/java/com/pro/baebooreung/chat/service/RedisSubcriber.java
+++ backend/src/main/java/com/pro/baebooreung/chat/service/RedisSubcriber.java
@@ -13,17 +13,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 @Service
+/**
+ * 레디스 구독 서비스 구현
+ * 레디스에 메시지 발행이 될 때까지 대기하였다가 메시지가 발행되면 해당 메시지를 읽어 처리
+ * 레디스에 메시지가 발행되면 해당 메시지를 Chat Message로 변환하고
+ * messaging Template을 이용하여 채팅방의 모든 websocket 클라이언트들에게 메시지 전달
+ */
 public class RedisSubcriber implements MessageListener {
 
     private final ObjectMapper objectMapper;
     private final RedisTemplate redisTemplate;
     private final SimpMessageSendingOperations messagingTemplate;
 
-    /**
-     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
-     *
-     */
-
+    //Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try{
