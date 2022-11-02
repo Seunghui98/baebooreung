9bcca2f225546ff545d72cb80d525cebf2975cb0
daseul <dew4325@naver.com>
2022-11-02 16:41:47 +0900

    [B]FIX_Chat Server

--- backend/chat/src/main/java/com/pro/baebooreung/chat/dto/ChatRoom.java
+++ backend/chat/src/main/java/com/pro/baebooreung/chat/dto/ChatRoom.java
@@ -13,6 +13,7 @@ public class ChatRoom implements Serializable {
     private static final long serialVersionUID = 6494678977089006639L;
     private String roomId;
     private String name;
+    private long userCount;
 
     public static ChatRoom create(String name){
         ChatRoom chatRoom = new ChatRoom();
--- backend/chat/src/main/java/com/pro/baebooreung/chat/repository/ChatRoomRepository.java
+++ backend/chat/src/main/java/com/pro/baebooreung/chat/repository/ChatRoomRepository.java
@@ -1,48 +1,42 @@
 package com.pro.baebooreung.chat.repository;
 
 import com.pro.baebooreung.chat.dto.ChatRoom;
-import com.pro.baebooreung.chat.service.RedisSubcriber;
+import com.pro.baebooreung.chat.service.RedisSubscriber;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import java.util.*;
 
 @RequiredArgsConstructor
-@Repository
+@Service
 public class ChatRoomRepository {
 
-    //채팅방(topic)에 발행되는 메시지를 처리할 Listner
-     private final RedisMessageListenerContainer redisMessageListener;
+     //Redis CacheKeys
+     private static final String CHAT_ROOMS = "CHAT_ROOM"; //채팅룸 저장
+     private static final String USER_COUNT = "USER_COUNT"; //채팅룸에 입장한 클라이언트 수 저장
+     private static final String ENTER_INFO = "ENTER_INFO"; //채팅룸에 입장한 클라이언트의 sessionId와 채팅룸 id를 맵핑한 정보 저장
 
-     //구독 처리 서비스
-     private final RedisSubcriber redisSubcriber;
-
-     //Redis
-     private static final String CHAT_ROOMS = "CHAT_ROOM";
-     private final RedisTemplate<String, Object> redisTemplate;
-     private HashOperations<String, String, ChatRoom> opsHashChatRoom;
-
-     //채팅방의 대화 메시지를 발행하기 위한 redis topic 정보. 서버별로 채팅방에 매치되는
-     //topic 정보를 Map에 넣어 roomId로 찾을 수 있도록 한다.
-     private Map<String, ChannelTopic> topics;
-
-     @PostConstruct
-     private void init (){
-         opsHashChatRoom = redisTemplate.opsForHash();
-         topics = new HashMap<>();
-     }
+     @Resource(name = "redisTemplate")
+     private HashOperations<String, String, ChatRoom> hashOpsChatroom;
+     @Resource(name = "redisTemplate")
+     private HashOperations<String, String, String> hashOpsEnterInfo;
+     @Resource(name = "redisTemplate")
+     private ValueOperations<String, String> valueOps;
 
      public List<ChatRoom> findAllRoom(){
-         return opsHashChatRoom.values(CHAT_ROOMS);
+         return hashOpsChatroom.values(CHAT_ROOMS);
      }
 
      public ChatRoom findRoomById(String id){
-            return opsHashChatRoom.get(CHAT_ROOMS, id);
+            return hashOpsChatroom.get(CHAT_ROOMS, id);
         }
 
     /**
@@ -50,25 +44,54 @@ public class ChatRoomRepository {
      */
     public ChatRoom createChatRoom(String name){
             ChatRoom chatRoom = ChatRoom.create(name);
-            opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+            hashOpsChatroom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
             return chatRoom;
      }
 
     /**
-     * 채팅방 입장 : redis에 topic을 만들고 pub/sub 통신을 하기 위해 리스너를 설정한다.
+     * 유저가 입장한 채팅방 id와 유저 세션 id 맵핑 정보 저장
      */
-    public void enterChatRoom(String roomId){
-        ChannelTopic topic = topics.get(roomId);
-        if(topic == null) {
-            topic = new ChannelTopic(roomId);
-            redisMessageListener.addMessageListener(redisSubcriber, topic);
-            topics.put(roomId, topic);
-        }
+    public void setUserEnterInfo(String sessionId, String roomId){
+        hashOpsEnterInfo.put(ENTER_INFO, sessionId, roomId);
     }
 
-    public ChannelTopic getTopic(String roomId){
-        return topics.get(roomId);
+    /**
+     * 유저 세션으로 입장해 있는 채팅방 id 조회
+     */
+    public String getUserEnterRoomId(String sessionId){
+        return hashOpsEnterInfo.get(ENTER_INFO, sessionId);
     }
+
+    /**
+     * 유저 세션정보와 맵핑된 채팅방 id 삭제
+     */
+    public void removeUserEnterInfo(String sessionId){
+        hashOpsEnterInfo.delete(ENTER_INFO, sessionId);
+    }
+
+    /**
+     * 채팅방 유저 수 조회
+     */
+    public long getUserCount(String roomId){
+        return Long.valueOf(Optional.ofNullable(valueOps.get(USER_COUNT + "_" + roomId)).orElse("0"));
+    }
+
+    /**
+     * 채팅방에 입장한 유저 수 +1
+     */
+    public long plusUserCount(String roomId){
+        return Optional.ofNullable(valueOps.increment(USER_COUNT + "_" + roomId)).orElse(0L);
+    }
+
+    /**
+     * 채팅방에 입장한 유저 수 -1
+     */
+    public long minusUserCount(String roomId){
+        return Optional.ofNullable(valueOps.decrement(USER_COUNT + "_" + roomId)).filter(count -> count > 0).orElse(0L);
+    }
+
+
+
 
 
 }
