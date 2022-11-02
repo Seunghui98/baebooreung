0b14323b634dfa49e7ff0652fc7ea94426660ef1
daseul <dew4325@naver.com>
2022-11-02 16:41:47 +0900

    [B]FIX_Chat Server

--- backend/chat/src/main/java/com/pro/baebooreung/chat/dto/ChatMessage.java
+++ backend/chat/src/main/java/com/pro/baebooreung/chat/dto/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.pro.baebooreung.chat.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,12 +12,26 @@ public class ChatMessage {
      * 메세지 타입
      * ENTER : 입장
      * TALK : 대화
+     * QUIT : 퇴장
      */
-    public enum MessageType{
-        ENTER, TALK, JOIN
+
+    public ChatMessage(){
     }
-    private MessageType type;
-    private String roomId;
-    private String sender;
-    private String message;
+
+    @Builder
+    public ChatMessage(MessageType type, String roomId, String sender, String message, long userCount) {
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+        this.userCount = userCount;
+    }
+    public enum MessageType{
+        ENTER, TALK, QUIT
+    }
+    private MessageType type; //메세지 타입
+    private String roomId; //방번호
+    private String sender; //메시지 보낸 사람
+    private String message; // 메시지
+    private long userCount; //채팅방 인원 수, 채팅방 내에서 미시지가 전달될때 인원수 갱신시 사용
 }
