3d36347c34fd2bacd1b1bcf8be9bcf2a6852c957
junojam <nak3652@naver.com>
2022-11-03 01:42:06 +0900

    [F] ADD assets/images

--- frontend/baebooreung/src/MainPage/MainFrame.js
+++ frontend/baebooreung/src/MainPage/MainFrame.js
@@ -1,21 +1,42 @@
 import styles from './MainFrame.module.css'
 import React, { useEffect } from 'react';
-import logo from '../assets/iamges/배달긱.png'
+import logo from '../assets/images/logo_short.png'
+import dashboard from '../assets/images/dashboard.png'
+import notice from '../assets/images/logo_short.png'
+import map from '../assets/images/map.png'
+import chat from '../assets/images/chat.png'
+import logout from '../assets/images/logout.png'
 
 
-const MainFrame = ({SetMainId}) => {
+const MainFrame = ({ SetMainId }) => {
 
   return (
-    <div className={styles.main_display_flex}  style={{height:"100%"}}>
+    <div className={styles.main_display_flex} style={{ height: "100%" }}>
       <div className={styles.main_justify_content_space_between}>
         <div className={styles.main_display_flex_direction_col}>
           <img className={styles.main_logo_image} src={logo} alt="" />
-          <button onClick={() => {SetMainId(0)}}>대시보드</button>
-          <button onClick={() => {SetMainId(1)}}>채팅</button>
-          <button onClick={() => {SetMainId(2)}}>드라이버위치</button>
+          <button className={styles.button_style} onClick={() => { SetMainId(0) }}>
+            <img className={styles.main_frame_logo_image} src={dashboard} alt="" />
+            &nbsp;&nbsp;&nbsp;대시보드
+          </button>
+          <button className={styles.button_style} onClick={() => { SetMainId(1) }}>
+            <img className={styles.main_frame_logo_image} src={map} alt="" />
+            &nbsp;&nbsp;&nbsp;드라이버위치
+          </button>
+          <button className={styles.button_style} onClick={() => { SetMainId(2) }}>
+            <img className={styles.main_frame_logo_image} src={notice} alt="" />
+            &nbsp;&nbsp;&nbsp;공지사항
+          </button>
+          <button className={styles.button_style} onClick={() => { SetMainId(3) }}>
+            <img className={styles.main_frame_logo_image} src={chat} alt="" />
+            &nbsp;&nbsp;&nbsp;채팅
+          </button>
         </div>
         <div className={styles.main_display_flex_direction_col}>
-          <button>내 이름</button>
+          <button>
+            &entp;내 이름&entp;
+            <img className={styles.main_frame_logo_image} src={logout} alt="" />
+          </button>
         </div>
       </div>
       <div className={styles.main_content}>
