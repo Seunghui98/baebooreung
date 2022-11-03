e540eb5bc0e7d61bd0ceaa164b1523c51e23212a
junojam <nak3652@naver.com>
2022-11-04 05:51:52 +0900

    [F] TEST front build test

---
+++ frontend/baebooreung/Page404.js
@@ -0,0 +1,8 @@
+import styles from'./Page404.module.css'
+
+const Page404 = () => {
+return <div className={styles.age404}>
+    404 Page
+  </div>;
+};
+export default Page404;
---
+++ frontend/baebooreung/mainpagesub/Notice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notice = () => {
+  return (
+    <div>
+      공지사항 위치입니다.
+    </div>
+  )
+}
+
+export default Notice;
