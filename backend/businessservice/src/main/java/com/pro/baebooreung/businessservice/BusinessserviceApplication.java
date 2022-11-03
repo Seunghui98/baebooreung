09f914bbb25773fb69430f38b3bb1e13daf0ef12
LeeJieuni <lgeun123@naver.com>
2022-11-04 07:39:07 +0900

    ADD_business service

---
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/BusinessserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pro.baebooreung.businessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessserviceApplication.class, args);
+	}
+
+}
---
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/Route.java
@@ -0,0 +1,41 @@
+package com.pro.baebooreung.businessservice.domain;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "route")
+public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @Column(name = "user_id")
+    private int user_id; //사용자 id(토큰관련아니고 리얼 id)
+
+    @Column(name = "delivery_datetime", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime deliveryDateTime;
+
+    @Column(name = "route_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RouteType routeType;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "delivery",cascade = CascadeType.ALL)
+    private List<Delivery> deliveryList = new ArrayList<>();
+}
