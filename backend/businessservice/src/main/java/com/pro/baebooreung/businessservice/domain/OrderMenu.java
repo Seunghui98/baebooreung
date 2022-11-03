92c255fd1e017a9d2e3424eac707843e1a65a4b3
LeeJieuni <lgeun123@naver.com>
2022-11-04 07:39:07 +0900

    ADD_business service

---
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/OrderMenu.java
@@ -0,0 +1,31 @@
+package com.pro.baebooreung.businessservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "ordermenu")
+public class OrderMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name="menu", nullable = false)
+    private String menu;
+
+    @Column(name="number", nullable = false)
+    private int number;
+}
