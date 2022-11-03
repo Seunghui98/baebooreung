0499fe1c1098605c06a146b461457ae956a72e9a
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-11-04 02:28:47 +0900

    [F] FIX CSS at HomeScreen

--- frontend/app/src/components/MangaerHome.js
+++ frontend/app/src/components/MangaerHome.js
@@ -6,21 +6,21 @@ const {height : SCREEN_HEIGHT, width : SCREEN_WIDTH} = Dimensions.get('window');
 function ManagerHome({navigation}){
     const user = { name : "최싸피", grade : "관리자" }
     //const user = useSelector((state) => state.user.value); //react-redux를 이용하여 user 정보 받을 예정
-    const workList = [{region : 1, regionName : "광주과학기술원"},{region : 2, regionName : "전남대학교"},{region :3, regionName : "서울대학교"},{region :4, regionName : "연세대학교"},{region :5, regionName : "건국대학교"},{region :6, regionName : "경희대학교"}]
+    const workList = [{region : 1, regionName : "광주과학기술원", driver :2, total : 50, finish :30},{region : 2, regionName : "전남대학교",driver :2,total : 25, finish :25},{region :3, regionName : "서울대학교",driver :5,total : 70, finish :50},{region :4, regionName : "연세대학교",driver :3,total : 50, finish :50},{region :5, regionName : "건국대학교",driver :3,total : 45, finish :25},{region :6, regionName : "경희대학교",driver :2,total : 50, finish :30}]
     return(
         <View style={styles.container}>
             <View style={styles.top}>
                 <Icon name="person" size={SCREEN_HEIGHT/8} color={'#F4FA58'}></Icon>
                 <View style={styles.topTextLayout}>
-                    <Text style={styles.topText}> {user.name} {user.grade}님 안녕하세요!</Text>
+                    <Text style={styles.topText}> {user.name} {user.grade}님</Text>
                 </View>
             </View>
 
-            <View style={styles.noticeLayout}>
+            {/* <View style={styles.noticeLayout}>
                 <View style={styles.notice}>
                     <Text style={styles.noticeText}>공지사항</Text>
                 </View>
-            </View>
+            </View> */}
 
             <View style={styles.dailyWorkLayout}>
                 <View style={styles.dailyWorkTitleTextLayout}>
@@ -32,14 +32,33 @@ function ManagerHome({navigation}){
                     data={workList}
                     keyExtractor={item => item.region}
                     renderItem={({item}) =>
+                    <TouchableOpacity
+                    onPress={()=>{
+                        navigation.navigate('DetailWork',{ headerTitle : `${item.regionName}`, region : item.region, driver : item.driver, total : item.total, finish : item.finish})
+                    }}>
                         <View style={styles.dailyWorkList}>
-                        <TouchableOpacity
-                        onPress={()=>{
-                            navigation.navigate('DetailWork',{ headerTitle : `${item.regionName}`, region : item.region})
-                        }}>
-                        <Text style={styles.dailyWorkListText}>{item.regionName}</Text>
-                        </TouchableOpacity>
+                            <View style={styles.dailWorkFirstLine}>
+                                <View style={{flex :1}}>
+                                    <Text style={styles.dailyWorkListText}>{item.regionName}</Text>
+                                </View>
+                                <View style={{flex :1, flexDirection :'row',justifyContent : 'flex-end'}}>
+                                    {item.total === item.finish &&
+                                        <Text style={styles.dailyWorkFinishText}>완료</Text>
+                                    }
+                                    {item.total !== item.finish &&
+                                    <View style={{flex :1, flexDirection :'row', justifyContent :'flex-end'}}>
+                                        <Text style={styles.dailyWorkText}>{item.finish} </Text>
+                                        <Text style={{fontWeight :'bold'}}>/ {item.total}</Text>
+                                    </View>
+                                    }
+                                    
+                                </View>
+                            </View>
+                            <View>
+                                <Text style={styles.driverNumText}>드라이버 {item.driver}명</Text>
+                            </View>
                         </View>
+                    </TouchableOpacity>
                     }
                 >
                 </FlatList>
@@ -55,7 +74,6 @@ const styles = StyleSheet.create({
     },
     top : {
         flexDirection : 'row',
-        width : SCREEN_WIDTH,
         height : SCREEN_HEIGHT/8,
         backgroundColor :'#191970',
         shadowOffset : {width:0, height:1},
@@ -65,7 +83,7 @@ const styles = StyleSheet.create({
     },
     topTextLayout : {
         flex :1,
-        alignItems : 'center',
+        alignItems : 'flex-start',
         justifyContent : 'center',
         
     },
@@ -98,17 +116,18 @@ const styles = StyleSheet.create({
         fontWeight :'bold'
     },
     dailyWorkLayout : {
-        width : SCREEN_WIDTH,
-        height : SCREEN_HEIGHT*5/8,
+        flex :1
     },
     dailyWorkTitleTextLayout : {
-        alignItems : 'center',
+        alignItems : 'flex-start',
+        paddingLeft :30,
+        paddingTop : 15,
         justifyContent : 'center',
 
     },
     dailyWorkTitleText : {
-        fontSize : 22,
-        fontWeight :'bold'
+        fontSize : 17,
+        fontWeight : '900'
     },
     dailyWork : {
         flex : 1,
@@ -121,19 +140,32 @@ const styles = StyleSheet.create({
         marginVertical : 15,
     },
     dailyWorkList : {
+        flex :1,
         height : SCREEN_HEIGHT/10,
-        alignItems : 'center',
-        justifyContent :'center',
-        borderRadius : 20, 
+        justifyContent :'space-between',
+        paddingHorizontal:10,
+        paddingVertical : 10,
+        borderRadius : 10, 
         backgroundColor :'white',
         marginBottom :20,
         shadowOffset : {width:0, height:1},
         shadowRadius :2,
-        elevation :10, 
+        elevation :2, 
         shadowOpacity :0.4,
     },
+    dailWorkFirstLine:{
+        flex :1,
+        flexDirection : 'row',
+    },
     dailyWorkListText : {
-        fontSize : 22,
+        fontSize : 14,
+        fontWeight :'bold'
+    },
+    dailyWorkFinishText : {
+        color : 'green'
+    },
+    dailyWorkText : {
+        color : 'red'
     }
 });
 export default ManagerHome;
--- frontend/app/src/pages/managerApp/DetailWorkScreen.js
+++ frontend/app/src/pages/managerApp/DetailWorkScreen.js
@@ -3,13 +3,20 @@ import {Text, View,StyleSheet} from 'react-native';
 import DetailWork from '../../components/DetailWork';
 
 function DetailWorkScreen(props){
+    console.log(props)
     useEffect(()=>{
         const headerTitle = props.route.params.headerTitle;
         props.navigation.setOptions({headerTitle : headerTitle});
     },[])
     return(
         <View style={styles.container}>
-            <DetailWork regionName={props.route.params.headerTitle} region={props.route.params.region}></DetailWork>
+            <DetailWork 
+                regionName={props.route.params.headerTitle} 
+                region={props.route.params.region}
+                driver={props.route.params.driver}
+                total={props.route.params.total}
+                finsih={props.route.params.finish}
+            ></DetailWork>
         </View>
 
     )
