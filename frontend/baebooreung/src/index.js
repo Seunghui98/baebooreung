73cab5dc57e03f6bab1438e5c96e8e879661fa6e
junojam <nak3652@naver.com>
2022-11-04 09:39:53 +0900

    [F] TEST front build test

--- frontend/baebooreung/src/index.js
+++ frontend/baebooreung/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './App';
-import { RenderAfterNavermapsLoaded } from 'react-naver-maps'
+// import { RenderAfterNavermapsLoaded } from 'react-naver-maps'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RenderAfterNavermapsLoaded clientId={"i3oq00t777"}>
-    <App />
-  </RenderAfterNavermapsLoaded>
+  <App />
+  // <RenderAfterNavermapsLoaded clientId={"i3oq00t777"}>
+  // </RenderAfterNavermapsLoaded>
 );
 
 // If you want to start measuring performance in your app, pass a function
