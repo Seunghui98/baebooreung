41d25d84b9969168ea1f90174dc8068a31863923
junojam <nak3652@naver.com>
2022-11-04 17:10:40 +0900

    [F] STYLE admin page

--- frontend/baebooreung/src/mainpagesub/MainPage.js
+++ frontend/baebooreung/src/mainpagesub/MainPage.js
@@ -7,6 +7,8 @@ import DashBoard from "./DashBoard";
 import Chatting from "./Chatting";
 import Notice from "./Notice";
 import NaverMapApi from '../navermap/NaverMapApi';
+import fold_on from '../assets/images/fold_on.png'
+import fold_off from '../assets/images/fold_off.png'
 
 const MainPage = () => {
   // console.log('')
@@ -30,21 +32,24 @@ const MainPage = () => {
     3: <Chatting />
   }
   const menu_header = {
-    0: 'DASH BOARD',
-    1: 'DRIVER LOCATION',
-    2: 'NOTICE',
-    3: 'CHATTING'
+    0: '대시보드',
+    1: '드라이버 위치',
+    2: '공지사항',
+    3: '채팅'
   }
 
   return (
     <div style={{ height: "100%" }}>
-      <div style={{ display: "flex" }}>
-        <div>
+      {/* <button onClick={fold} className={styles.fold_button}>
+        <img src={fold_on} style={{width:"20px", height:"20px"}} alt="" />
+      </button> */}
+      <div style={{ display: "flex", height: "100%" }}>
+        <div style={{ display: "flex", height: "100%"}}>
           <MainFrame setMainId={clickhandler} changeMenuHeader={clickhandler2} />
         </div>
-        <div style={{ width: "100%" }}>
+        <div style={{ width:"100%" }}>
           <div className={styles.menu_header_after} id="menu_header">{menu_header[MenuId]}</div>
-          {contents[MainId]}
+          <div className={styles.menu_content}>{contents[MainId]}</div>
         </div>
       </div>
     </div>
