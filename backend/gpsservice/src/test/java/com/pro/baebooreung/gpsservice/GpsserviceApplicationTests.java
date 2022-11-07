b386897bcff0c8b9b5f31f5fe2ef29a0431ea8c9
LeeSeungHui <hithere1012@naver.com>
2022-11-07 17:37:53 +0900

    [B] ADD_gps service
    실시간 배달원의 gps정보 반환하는 API
    #S07P31C207-102

---
+++ backend/gpsservice/src/test/java/com/pro/baebooreung/gpsservice/GpsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.baebooreung.gpsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
