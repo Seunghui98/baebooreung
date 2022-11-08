ca0af8502ee4007aad8337b348c6d752739e1245
leejieun <lgeun123@naver.com>
2022-11-08 11:26:43 +0900

    [B]update agpigateway delete others

--- backend/consumerservice/src/main/java/com/pro/baebooreung/consumerservice/domain/Grade.java
+++
@@ -1,8 +0,0 @@
-package com.pro.baebooreung.consumerservice.domain;
-
-public enum Grade {
-    UNAUTHORIZED, // 권한받지 않은 드라이버
-    DRIVER,  // 권한 받은 드라이버
-    MANAGER,  // 관리자
-    ADMIN  // 총관리자(EX. 개발자)
-}
