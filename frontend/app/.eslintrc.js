7d3452f48f7043828dd941e767d18ef3caa36f8f
leejieun <lgeun123@naver.com>
2022-11-08 11:30:59 +0900

    [B]update discovery delete others

--- frontend/app/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  extends: ['airbnb', 'prettier'],
-  rules: {
-      'import/prefer-default-export': 'off',
-      'import/extensions': ['off'],
-   }
-};
