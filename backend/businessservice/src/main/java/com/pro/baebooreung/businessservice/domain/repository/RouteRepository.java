6b11af70becb80f4763a9cb1c831ff1d7edf1588
leejieun <lgeun123@naver.com>
2022-11-08 17:11:59 +0900

    [B]FIX_business getRouteByUserNDate

--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/repository/RouteRepository.java
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/repository/RouteRepository.java
@@ -2,11 +2,14 @@ package com.pro.baebooreung.businessservice.domain.repository;
 
 import com.pro.baebooreung.businessservice.domain.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 
 public interface RouteRepository extends JpaRepository<Route,Integer> {
     Iterable<Route> findByUserId(int userId);
-    Iterable<Route> findByUserIdAndDeliveryDateTime(int userId, LocalDate date);
+    @Query("SELECT r FROM Route as r WHERE r.userId = :userId and r.deliveryDateTime = :dateTime")
+    Iterable<Route> findByUserIdAndDeliveryDateTime(@Param("userId") int userId, @Param("datetime")LocalDate date);
 }
--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/service/RouteService.java
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/service/RouteService.java
@@ -3,11 +3,15 @@ package com.pro.baebooreung.businessservice.service;
 import com.pro.baebooreung.businessservice.domain.Route;
 import com.pro.baebooreung.businessservice.vo.ResponseRoute;
 import lombok.Data;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RouteService {
 //    public Iterable<Route> getRouteByUserNDate(int userId);//, Data date);
+
     public List<ResponseRoute> getRouteByUserNDate(int userId);//, Data date);
     public List<ResponseRoute> getRouteByUser(int userId);
 }
