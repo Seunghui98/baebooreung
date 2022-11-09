0c4b460522b2e82537f4d7c6aff1ca148f5e8554
LeeJieuni <lgeun123@naver.com>
2022-11-10 00:10:07 +0900

    [B]UPDATE_gps delete others

--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/vo/RequestCheckIn.java
+++
@@ -1,9 +0,0 @@
-package com.pro.baebooreung.businessservice.vo;
-
-import lombok.Data;
-
-@Data
-public class RequestCheckIn {
-    private int routeId;
-    private int deliveryId;
-}
--- backend/consumerservice/src/main/java/com/pro/baebooreung/consumerservice/domain/WorkStatus.java
+++
@@ -1,5 +0,0 @@
-package com.pro.baebooreung.consumerservice.domain;
-
-public enum WorkStatus {
-    DRIVING, OFF
-}
