680cafba43628fcfd8d4eef48e253cedaa8a029f
LeeJieuni <lgeun123@naver.com>
2022-11-10 00:35:23 +0900

    [B]ADD_gps msa

--- backend/gpsservice/src/main/java/com/pro/baebooreung/gpsservice/GpsserviceApplication.java
+++ backend/gpsservice/src/main/java/com/pro/baebooreung/gpsservice/GpsserviceApplication.java
@@ -2,8 +2,12 @@ package com.pro.baebooreung.gpsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient //유레카 서버에 등록
+@EnableFeignClients //Feign클라이언트로 등록
 public class GpsserviceApplication {
 
 	public static void main(String[] args) {
