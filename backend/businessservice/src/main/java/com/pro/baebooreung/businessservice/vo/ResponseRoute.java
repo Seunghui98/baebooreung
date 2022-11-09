550b8459a97e4ef687732753029be1b76493837d
leejieun <lgeun123@naver.com>
2022-11-09 15:06:00 +0900

    [B]UPDATE_business responseRoute

--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/vo/ResponseRoute.java
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/vo/ResponseRoute.java
@@ -1,9 +1,12 @@
 package com.pro.baebooreung.businessservice.vo;
 
+import com.pro.baebooreung.businessservice.domain.RouteType;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Data
@@ -11,6 +14,11 @@ import java.util.List;
 public class ResponseRoute {
     private int id;
     private String routeName;
+    private RouteType routeType;
+    private boolean done;
+    private LocalDate date;
+    private LocalTime scheduledStartTime;
+    //private LocalTime actualStartTime; // 업무를 실제로 시작한 시간
 
     private List<ResponseDelivery> deliveryList;
 
