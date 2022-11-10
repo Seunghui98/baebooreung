f1fa415a62759a3f44eebf25656e737609c066cc
leejieun <lgeun123@naver.com>
2022-11-10 09:54:22 +0900

    [B]FIX_user test

--- backend/userservice/src/main/java/com/pro/baebooreung/userservice/domain/repository/UserRepository.java
+++ backend/userservice/src/main/java/com/pro/baebooreung/userservice/domain/repository/UserRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity,Integer> {
     UserEntity findBySpecialKey(String specialKey);
     UserEntity findByEmail(String username);
 
     UserEntity findById(int id);
+
+    @Override
+    <S extends UserEntity> S save(S entity);
 }
--- backend/userservice/src/main/java/com/pro/baebooreung/userservice/service/UserServiceImpl.java
+++ backend/userservice/src/main/java/com/pro/baebooreung/userservice/service/UserServiceImpl.java
@@ -33,6 +33,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+
 @Service
 public class UserServiceImpl implements UserService {
 
