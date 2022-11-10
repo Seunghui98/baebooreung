93b7c16b646aa683be24e6c50d9d359841c1861e
leejieun <lgeun123@naver.com>
2022-11-10 09:21:15 +0900

    [B]FIX_user transactional

--- backend/userservice/src/main/java/com/pro/baebooreung/userservice/domain/repository/UserRepository.java
+++ backend/userservice/src/main/java/com/pro/baebooreung/userservice/domain/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.pro.baebooreung.userservice.domain.repository;
 
 import com.pro.baebooreung.userservice.domain.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity,Integer> {
     UserEntity findBySpecialKey(String specialKey);
     UserEntity findByEmail(String username);
