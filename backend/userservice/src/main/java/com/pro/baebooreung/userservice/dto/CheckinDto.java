2d841bde41c00c720004140ecedfae65a71599dc
leejieun <lgeun123@naver.com>
2022-11-10 10:22:23 +0900

    [B]FIX_user sql show

--- backend/userservice/src/main/java/com/pro/baebooreung/userservice/dto/CheckinDto.java
+++ backend/userservice/src/main/java/com/pro/baebooreung/userservice/dto/CheckinDto.java
@@ -7,4 +7,13 @@ public class CheckinDto {
     private int id;
     private int routeId;
     private int deliveryId;
+
+    @Override
+    public String toString() {
+        return "CheckinDto{" +
+                "id=" + id +
+                ", routeId=" + routeId +
+                ", deliveryId=" + deliveryId +
+                '}';
+    }
 }
