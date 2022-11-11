e457d65b138ac8c5351eb0745bf0ad44addd77fd
leejieun <lgeun123@naver.com>
2022-11-11 15:15:32 +0900

    [B]ADD_user FCM

---
+++ backend/userservice/src/main/java/com/pro/baebooreung/userservice/dto/FCMDto.java
@@ -0,0 +1,10 @@
+package com.pro.baebooreung.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class FCMDto {
+    private String targetToken;
+    private String title;
+    private String body;
+}
