bbbe818ac5998b7b4b24a67c32ca973e2df4aa94
이승희 <hithere1012@naver.com>
2022-11-12 00:56:42 +0900

    [B] FIX_branch
    젠킨슨 개별 배포를 위한 다른 service 폴더 제거

--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/RouteType.java
+++
@@ -1,6 +0,0 @@
-package com.pro.baebooreung.businessservice.domain;
-
-public enum RouteType {
-    launch,
-    dinner
-}
--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/Type.java
+++
@@ -1,7 +0,0 @@
-package com.pro.baebooreung.businessservice.domain;
-
-public enum Type {
-    pickup,
-    hub, //이거 넣을까 말까
-    delivery
-}
