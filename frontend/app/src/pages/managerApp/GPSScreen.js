fd2d10a912dfde37e31f32eb2a197247902a4c3b
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-11-12 00:54:29 +0900

    [F] FIX ERR

--- frontend/app/src/pages/managerApp/GPSScreen.js
+++ frontend/app/src/pages/managerApp/GPSScreen.js
@@ -1,7 +1,4 @@
 import {Text, View} from 'react-native';
-import AudioRecord from '../../components/AudioRecord';
-import Swiper from 'react-native-swiper';
-import ManagerChat from '../../components/ManagerChat';
 
 function GPSScreen() {
   return <View>{/* <Gps></Gps> */}</View>;
