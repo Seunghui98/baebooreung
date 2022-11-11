19be8903a61afb04142c988aa0db9e6f6f642518
daseul <dew4325@naver.com>
2022-11-11 23:45:57 +0900

    [B]ADD_S3

--- backend/s3service/src/main/java/com/pro/baebooreung/s3service/S3serviceApplication.java
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/S3serviceApplication.java
@@ -3,9 +3,11 @@ package com.pro.baebooreung.s3service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@FeignClient
 public class S3serviceApplication {
 
 	public static void main(String[] args) {
---
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/dto/ProfileRequest.java
@@ -0,0 +1,15 @@
+package com.pro.baebooreung.s3service.dto;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileRequest {
+
+    MultipartFile image;
+    String userId;
+}
