02176e7f6c04fd2c5da381297a345f6957919a6b
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-11-13 11:10:43 +0900

    [F] 실제 데이터 연동
    드라이버가 가지고있는 루트ID와 루트ID안에 있는 루트들의 정보를 가지고 관리자의 업무현황페이지 실데이터 적용 (학교, 해당 학교에 배달하는 드라이버 ID,이름 및 드라이버 수, 총 배달 건수, 완료된 배달 건수)

--- frontend/app/src/components/MangaerHome.js
+++ frontend/app/src/components/MangaerHome.js
@@ -11,28 +11,233 @@ import {
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {setUser} from '../redux/user';
 import {setUserInfo} from '../redux/auth';
 import {setUserList} from '../redux/userList';
 import axios from 'axios';
-import {user} from '../api/api';
+import {business_service, user_service} from '../api/api';
 const {height: SCREEN_HEIGHT, width: SCREEN_WIDTH} = Dimensions.get('window');
 const identityColor = '#0B0B3B';
 const identityTextColor = '#FACC2E';
+const date = new Date();
+
 function ManagerHome({navigation}) {
   const dispatch = useDispatch();
-  const accessToken = useSelector(state => state.auth.accessToken);
   // const user = {name: '최싸피', grade: '관리자'};
   const userInfo = useSelector(state => state.user); //react-redux를 이용하여 user 정보 받을 예정
+  const userList = useSelector(state => state.userList.userList);
+  const [tempList, setTempList] = useState([]);
+  const [ok, setOk] = useState(false);
+  const [list, setList] = useState([]);
+  const [driverList, setDriverList] = useState([]);
+  const [university, setUniversity] = useState('');
+  const [total, setTotal] = useState(0);
+  const [finish, setFinish] = useState(0);
+  const [index, setIndex] = useState(0);
   const workList = [
-    {region: 1, regionName: '광주과학기술원', driver: 2, total: 50, finish: 30},
-    {region: 2, regionName: '전남대학교', driver: 2, total: 25, finish: 25},
-    {region: 3, regionName: '서울대학교', driver: 5, total: 70, finish: 50},
-    {region: 4, regionName: '연세대학교', driver: 3, total: 50, finish: 50},
-    {region: 5, regionName: '건국대학교', driver: 3, total: 45, finish: 25},
-    {region: 6, regionName: '경희대학교', driver: 2, total: 50, finish: 30},
+    {regionName: '광주과학기술원', driver: 2, total: 50, finish: 30},
+    {regionName: '전남대학교', driver: 2, total: 25, finish: 25},
+    {regionName: '서울대학교', driver: 5, total: 70, finish: 50},
+    {regionName: '연세대학교', driver: 3, total: 50, finish: 50},
+    {regionName: '건국대학교', driver: 3, total: 45, finish: 25},
+    {regionName: '경희대학교', driver: 2, total: 50, finish: 30},
   ];
+
+  useEffect(() => {
+    //userList(나를 제외한 유저리스트)가 갱신되었을 때 실행
+    if (userList !== null) {
+      // 먼저 유저리스트에서 grade가 DRIVER인 유저만 추출하여 드라이버가 가지고있는 루트ID를 추출
+      userList
+        .filter(item => item.grade === 'DRIVER')
+        .map((item, idx) => {
+          axios({
+            method: 'get',
+            url: business_service.getDriverRoute() + `${item.id}/` + 'routes/',
+          })
+            .then(res => {
+              console.log(
+                item.name,
+                '드라이버의 id를 통해 얻을 수 있는 정보',
+                res.data,
+              );
+
+              //추출된 루트ID를 가지고 그 루트ID가 가지고있는 경로 정보 모두 추출
+              if (res.data.length !== 0) {
+                res.data.map((el, idx) => {
+                  axios({
+                    method: 'get',
+                    url: business_service.getRoute() + `${el.id}`,
+                  })
+                    .then(result => {
+                      console.log(
+                        item.name,
+                        '드라이버가 저장한 루트id를 통해 얻을 수 있는 정보',
+                        result.data,
+                      );
+
+                      // 모든 루트 정보를 (userId, name, routeInfo)라는 속성을 가지는 새로운 객체배열에 저장
+                      setTempList(tempList => {
+                        const newTempList = [...tempList];
+                        newTempList.push({
+                          userId: item.email,
+                          name: item.name,
+                          routeInfo: result.data,
+                        });
+                        return newTempList;
+                      });
+                    })
+                    .catch(e => {
+                      console.log(e);
+                    })
+                    .finally(fin => {
+                      setOk(true);
+                    });
+                });
+              }
+            })
+            .catch(e => {
+              console.log(e);
+            });
+        });
+    }
+  }, [userList]);
+
+  useEffect(() => {
+    //현재 날짜를 받음
+    const realDate = toStringByFormatting(new Date());
+    console.log(realDate);
+    //현재 시간을 받음
+    const realTime = new Date().getHours();
+    console.log(realTime);
+    //모든 루트 정보가 저장되었을 시 실행
+    if (tempList.length !== 0) {
+      console.log(tempList);
+      tempList.map(item => {
+        //routeInfo의 done이 false일때만 진행
+        if (!item.routeInfo.done) {
+          //픽업 현재 진행여부 체크하여 저장(finish/total)
+          let totalPickupSum = 0;
+          let finishPickupSum = 0;
+          item.routeInfo.deliveryList
+            .filter(el => el.type === 'pickup')
+            .map(el => {
+              if (el.check === false) {
+                totalPickupSum += el.orderNum;
+              } else {
+                totalPickupSum += el.orderNum;
+                finishPickupSum += el.orderNum;
+              }
+            });
+          //배달 현재 진행여부 체크하여 저장(finish/total)
+          let totalDeliverySum = 0;
+          let finishDeliverySum = 0;
+          item.routeInfo.deliveryList
+            .filter(el => el.type === 'delivery')
+            .map(el => {
+              if (el.check === false) {
+                totalDeliverySum += el.orderNum;
+              } else {
+                totalDeliverySum += el.orderNum;
+                finishDeliverySum += el.orderNum;
+              }
+            });
+          //list에 대학교가 있는지 여부 판단 후 없으면 리스트에 객체 추가
+          //이때 배달기사는 하나의 학교(RouteName)에만 배달을 한다고 가정하고 진행
+          setUniversity(item.routeInfo.routeName);
+          setTotal(totalDeliverySum);
+          setFinish(finishDeliverySum);
+          const index = list.findIndex(find => {
+            find.routeName === item.routeInfo.routeName;
+          });
+          if (index === -1) {
+            setDriverList(driverList => {
+              const newDriverList = [...driverList];
+              newDriverList.push({userId: item.userId, name: item.name});
+              return newDriverList;
+            });
+            setIndex(index);
+          }
+          //학교(routerName)가 있다면 driver 목록에 있는지 여부 판단 후
+          //이미 존재하는 driver라면 driver는 추가하지 않고 원래있던 학교에 배달 진행여부만 체크하여 추가
+          //존재하지 않는 driver라면 driver도 추가
+          else {
+            let check = false;
+            list[index].driver.map(el => {
+              if (el.userId === item.userId) {
+                check = true;
+              }
+            });
+            //이미 존재하는 드라이버인 경우
+            if (check) {
+              setList(list => {
+                const newList = [...list];
+                newList[index].total += totalDeliverySum;
+                newList[index].finish += finishDeliverySum;
+                return newList;
+              });
+            }
+            //존재하지 않는 드라이버인 경우 드라이버 추가, 드라이버 숫자 업데이트
+            else {
+              setList(list => {
+                const newList = [...list];
+                newList[index].driver.push({
+                  userId: item.userId,
+                  name: item.name,
+                });
+                newList[index].driverNum += 1;
+                newList[index].total += totalDeliverySum;
+                newList[index].finish += finishDeliverySum;
+                return newList;
+              });
+            }
+          }
+        }
+      });
+    }
+  }, [ok]);
+
+  useEffect(() => {
+    //새로 추가되는 학교일 때 실행(list 배열에 새로운 객체 업데이트)
+    console.log('useEffec문 실행', index);
+    if (index === -1) {
+      setList(list => {
+        const newList = [...list];
+        newList.push({
+          routeName: university,
+          driver: [driverList[driverList.length - 1]],
+          driverNum: 1,
+          total: total,
+          finish: finish,
+        });
+        return newList;
+      });
+      setIndex(0);
+    }
+  }, [index]);
+
+  useEffect(() => {
+    if (list.length !== 0) {
+      console.log('list update');
+      console.log('업데이트 worklist', list);
+    }
+  }, [list]);
+
+  function leftPad(value) {
+    if (value >= 10) {
+      return value;
+    }
+    return `0${value}`;
+  }
+
+  function toStringByFormatting(source, delimiter = '-') {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+
+    return [year, month, day].join(delimiter);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.top}>
@@ -57,15 +262,15 @@ function ManagerHome({navigation}) {
         <View style={styles.dailyWork}>
           <FlatList
             style={styles.dailyWorkListLayout}
-            data={workList}
-            keyExtractor={item => item.region}
+            data={list}
+            keyExtractor={(item, idx) => idx}
             renderItem={({item}) => (
               <Pressable
                 onPress={() => {
                   navigation.navigate('DetailWork', {
                     headerTitle: `${item.regionName}`,
-                    region: item.region,
                     driver: item.driver,
+                    driverNum: item.driverNum,
                     total: item.total,
                     finish: item.finish,
                   });
@@ -78,7 +283,7 @@ function ManagerHome({navigation}) {
                     <View style={styles.dailWorkFirstLine}>
                       <View style={{flex: 1}}>
                         <Text style={styles.dailyWorkListText}>
-                          {item.regionName}
+                          {item.routeName}
                         </Text>
                       </View>
                       <View
@@ -109,7 +314,7 @@ function ManagerHome({navigation}) {
                     </View>
                     <View>
                       <Text style={styles.driverNumText}>
-                        드라이버 {item.driver}명
+                        드라이버 {item.driverNum}명
                       </Text>
                     </View>
                   </View>
