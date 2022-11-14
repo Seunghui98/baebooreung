c376bf8c3974d6d0909445e158927d4964afc301
daseul <dew4325@naver.com>
2022-11-14 11:28:55 +0900

    [B]FIX_MSA

--- backend/s3service/src/main/java/com/pro/baebooreung/s3service/S3serviceApplication.java
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/S3serviceApplication.java
@@ -3,11 +3,12 @@ package com.pro.baebooreung.s3service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@FeignClient
+@EnableFeignClients
 public class S3serviceApplication {
 
 	public static void main(String[] args) {
