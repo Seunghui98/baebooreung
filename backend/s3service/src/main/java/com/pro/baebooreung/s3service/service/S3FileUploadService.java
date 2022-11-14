c9dbf7e79d4a401fa2a18fd08d92710d481fa0f6
daseul <dew4325@naver.com>
2022-11-14 15:36:18 +0900

    [B]ADD_Resize Image

--- backend/s3service/src/main/java/com/pro/baebooreung/s3service/service/S3FileUploadService.java
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/service/S3FileUploadService.java
@@ -11,12 +11,18 @@ import com.pro.baebooreung.s3service.dto.ProfileRequest;
 import com.pro.baebooreung.s3service.dto.ProfileResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import marvin.image.MarvinImage;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.marvinproject.image.transform.scale.Scale;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -40,14 +46,16 @@ public class S3FileUploadService {
         MultipartFile curImage = profileReq.getImage();
 
         String originalName = LocalDate.now() + "/" + currentUserId + "/" + createFileName(curImage.getOriginalFilename());
-        long size = curImage.getSize();
+        MultipartFile resizedFile = resizeImage(curImage.getOriginalFilename(),originalName, curImage, 768);
+
+        long size = resizedFile.getSize();
 
         ObjectMetadata objectMetaData = new ObjectMetadata();
         objectMetaData.setContentType(curImage.getContentType());
         objectMetaData.setContentLength(size);
 
         amazonS3Client.putObject(
-                new PutObjectRequest(bucket, originalName, curImage.getInputStream(), objectMetaData)
+                new PutObjectRequest(bucket, originalName, resizedFile.getInputStream(), objectMetaData)
                         .withCannedAcl(CannedAccessControlList.PublicRead)
         );
 
@@ -68,6 +76,38 @@ public class S3FileUploadService {
             return fileName.substring(fileName.lastIndexOf("."));
         } catch (StringIndexOutOfBoundsException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일 : " + fileName);
+        }
+    }
+
+    MultipartFile resizeImage(String fileName, String fileFormatName, MultipartFile originalImage, int targetWidth) {
+        try {
+            // MultipartFile -> BufferedImage Convert
+            BufferedImage image = ImageIO.read(originalImage.getInputStream());
+            // newWidth : newHeight = originWidth : originHeight
+            int originWidth = image.getWidth();
+            int originHeight = image.getHeight();
+
+            // origin 이미지가 resizing될 사이즈보다 작을 경우 resizing 작업 안 함
+            if(originWidth < targetWidth)
+                return originalImage;
+
+            MarvinImage imageMarvin = new MarvinImage(image);
+
+            Scale scale = new Scale();
+            scale.load();
+            scale.setAttribute("newWidth", targetWidth);
+            scale.setAttribute("newHeight", targetWidth * originHeight / originWidth);
+            scale.process(imageMarvin.clone(), imageMarvin, null, null, false);
+
+            BufferedImage imageNoAlpha = imageMarvin.getBufferedImageNoAlpha();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(imageNoAlpha, fileFormatName, baos);
+            baos.flush();
+
+            return new MockMultipartFile(fileName, baos.toByteArray());
+
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 리사이즈에 실패했습니다.");
         }
     }
 
