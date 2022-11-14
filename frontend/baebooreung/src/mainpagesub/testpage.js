5ed4fcad704bdb0ecdbf86c9c732731b91827a39
junojam <nak3652@naver.com>
2022-11-15 00:01:22 +0900

    [F] CH explain for navermap

--- frontend/baebooreung/src/mainpagesub/testpage.js
+++ frontend/baebooreung/src/mainpagesub/testpage.js
@@ -7,6 +7,8 @@ const BASE_URL = "https://k7c207.p.ssafy.io:8000"
 
 
 const TestPage = (props) => {
+
+  // 지역, 날짜로 모든 경로 가져오기
   async function search_route() {
     await axios({
       url: BASE_URL + '/business-service/route/navigps',
@@ -22,36 +24,75 @@ const TestPage = (props) => {
     })
   }
 
+  // TO지은 -> 값을 보내기 위해서 리스트[126.8116, 35.2053]를 하나의 문자열('126.8116,35.2053')으로 바꿔야 함
   function make_LatLng(now_loc_temp) {
     return `${now_loc_temp.join(',')}`
   }
+  // 네이버 지도에서 두 점의 중심을 잡기 위한 함수
   function setTwoCenter(a, b) {
     return [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2]
   }
+  // 네이버 지도 줌
   const [zoom, setZoom] = useState(13)
+  // 네이버 지도 현재 위치 -> 중심을 지속적으로 옮기기 위함
   const [now_loc, setStart] = useState([126.8116, 35.2053]) // 전남대A출발지
 
+  // SSAFY 위도 경도
   const ssafyLatLng = [126.8116, 35.2053]
+  // CloudStone 위도 경도
   const cloudStoneLatLng = [126.85224, 35.14228]
+  // 중심 좌표
   const [center, setCenter] = useState(setTwoCenter(ssafyLatLng, cloudStoneLatLng))
+
+  // 경유지가 있다고 임의로 가정
+  let waypoints = [
+    [126.8982, 35.1786], // 킹스샌드
+    [126.9043, 35.1777], // 알촌
+    [126.9030, 35.1777], // 봉구스밥버거
+    [126.9028, 35.1779], // 카츠앤맘
+    [126.9021, 35.1774], // 덮덮밥 광주용봉점
+    [126.9011, 35.1779], // 마라미녀
+    [126.9012, 35.1798], // 오늘하루가
+    [126.9029, 35.1807], // 김밥나라
+    [126.9101, 35.1813], // 생핼관4동입구
+    [126.9109, 35.1802], // 생핼관6동입구
+    [126.8997, 35.1765], // 8동생활관콜라자판기옆
+    [126.9108, 35.1804]  // 생활관5동입구
+  ]
+  // 결과적으로 waypoints는 126.8982,35.1786|126.9043,35.1777|126.9030,35.1777:의 형태를 나타내게 된다.
+  // 경유지 좌표를 위도, 경도 순서로 잇고, |를 구분자로 정하고, 마지막을 :로 정리한다.
+  function make_waypoints(waypoints_temp) {
+    return waypoints_temp.join('|') + ':'
+  }
+
+
+  // TO지은 -> 값을 보내는 REQUEST BODY
   const ssafy_cloudstone_route_temp = {
     start: make_LatLng(ssafyLatLng),
     goal: make_LatLng(cloudStoneLatLng),
-    option: "trafast"
+    option: "trafast", // trafast는 traffic fast 가장 빠른 길이라는 뜻
+    // waypoints: make_waypoints(waypoints) // 경유지는 임시 주석 처리, 사용가능한 상태임.
   }
+  // SSAFY부터 CloudStone까지 모든 경로를 담을 리스트
   const [SsafyCloudStoneCourse, setSsafyCloudStoneCourse] = useState([])
 
-  async function cal_course(route) {
+  // 경로 구하는 API 적용, date에는 requestBody를 넣을 것임
+  async function cal_course(requestBody) {
+    // 빈 course 선언
     const course = []
+    // TO지은 -> 해당 API URL, post 주의
     await axios({
       url: "https://k7c207.p.ssafy.io:8000/user-service/map",
       method: "post",
-      data: route,
+      data: requestBody,
     }).then((res) => {
+      // requestBody를 통해 경유지가 포함된 모든 경로를 계산한 값이 저장되는 path
       const path = res.data.route.trafast[0].path
+      // path의 모든 값들을 빈 배열인 course에 push 한다.
       for (let i = 0; i <= path.length - 1; i++) {
         course.push(new naver.maps.LatLng(path[i][1], path[i][0]))
       }
+      // push된 값을 53줄의 모든 경로를 담을 리스트에 넣는다.
       setSsafyCloudStoneCourse(course)
     })
   }
