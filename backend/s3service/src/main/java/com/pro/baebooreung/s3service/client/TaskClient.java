4821d5d55fd74bc86db9a1e67fead20f19bf8923
daseul <dew4325@naver.com>
2022-11-14 16:25:07 +0900

    [B]FIX_API

--- backend/s3service/src/main/java/com/pro/baebooreung/s3service/client/TaskClient.java
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/client/TaskClient.java
@@ -3,6 +3,7 @@ package com.pro.baebooreung.s3service.client;
 import com.pro.baebooreung.s3service.dto.ProfileResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +13,6 @@ public interface TaskClient {
     @PostMapping("/user/save/profile")
     void saveProfile(ProfileResponse res);
 
-    @GetMapping("/user/profile")
-    String getProfile(int userId);
+    @GetMapping("/user/profile/{userId}")
+    String getProfile(@PathVariable("userId") int userId);
 }
