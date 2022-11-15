412bff3625c82e5356cf1bfcbc0f00c07f010f48
daseul <dew4325@naver.com>
2022-11-15 10:00:10 +0900

    [B]FIX

--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/service/DeliveryServiceImpl.java
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/service/DeliveryServiceImpl.java
@@ -5,6 +5,9 @@ import com.pro.baebooreung.businessservice.domain.repository.DeliveryRepository;
 import com.pro.baebooreung.businessservice.dto.CheckResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,9 +18,21 @@ import java.util.Optional;
 public class DeliveryServiceImpl implements DeliveryService {
 
     DeliveryRepository deliveryRepository;
+    BCryptPasswordEncoder passwordEncoder;
+
+    Environment env;
+    @Autowired
+    public DeliveryServiceImpl(DeliveryRepository deliveryRepository,
+                           BCryptPasswordEncoder passwordEncoder,
+                           Environment env ) {
+        this.deliveryRepository= deliveryRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.env = env;
+    }
 
     @Override
     public void saveImg(CheckResponse res) {
+        log.info(res.getDelId()+":delivery Id");
         Optional<Delivery> findDel = deliveryRepository.findById(res.getDelId());
         Delivery delivery = findDel.get();
         log.info(delivery+":delivery!!!!!!!!!");
