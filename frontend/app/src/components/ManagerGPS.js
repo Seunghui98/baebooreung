a26f34896b236c20e2eb40ef00e849c4f0ee5635
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-11-15 09:54:32 +0900

    [F] ManagerGPS 페이지 수정(실제데이터 적용관련)

--- frontend/app/src/components/ManagerGPS.js
+++ frontend/app/src/components/ManagerGPS.js
@@ -20,7 +20,6 @@ export default function ManagerGPS({navigation}) {
   const userInfo = useSelector(state => state.user);
   const userList = useSelector(state => state.userList.userList);
   const [tempList, setTempList] = useState([]);
-  const [ok, setOk] = useState(false);
   const [list, setList] = useState([]);
   const [driverList, setDriverList] = useState([]);
   const [university, setUniversity] = useState('');
@@ -30,15 +29,13 @@ export default function ManagerGPS({navigation}) {
   const [deliveryFinish, setDeliveryFinish] = useState(0);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
+  const setInfoList = async () => {
     //userList(나를 제외한 유저리스트)가 갱신되었을 때 실행
     if (userList !== null) {
       //현재 날짜를 받음
       const realDate = toStringByFormatting(new Date());
-      console.log(realDate);
       //현재 시간을 받음
       const realTime = new Date().getHours();
-      console.log(realTime);
       //오후 10시부터 오후 2시까지는 점심정보를 띄움,오후 2시부터 오후 10시까지는 저녁정보를 띄움
 
       let realType; //점심, 저녁에 대한 정보를 시간에 따라 저장
@@ -51,43 +48,43 @@ export default function ManagerGPS({navigation}) {
       // 먼저 유저리스트에서 grade가 DRIVER인 유저만 추출하여 드라이버가 가지고있는 루트ID를 추출
       userList
         .filter(item => item.grade === 'DRIVER')
-        .map((item, idx) => {
-          axios({
+        .map(async (item, idx) => {
+          await axios({
             method: 'get',
             url: business_service.getDriverRoute() + `${item.id}/` + 'routes/',
           })
-            .then(res => {
-              console.log(
-                item.name,
+            .then(async res => {
+              // console.log(
+              //   item.name,
+              //   '드라이버의 id를 통해 얻을 수 있는 정보',
+              //   res.data,
+              // );
 
-                '드라이버의 id를 통해 얻을 수 있는 정보',
-                res.data,
-              );
-
-              //추출된 루트ID를 가지고 그 루트ID가 가지고있는 경로 정보 모두 추출
               if (res.data.length !== 0) {
-                res.data.map((el, idx) => {
+                //추출된 루트ID를 가지고 그 루트ID가 가지고있는 경로 정보 모두 추출
+                res.data.map(async (el, idx) => {
                   axios({
                     method: 'get',
                     url: business_service.getRoute() + `${el.id}`,
                   })
                     .then(result => {
-                      console.log(
-                        item.name,
-                        '드라이버가 저장한 루트id를 통해 얻을 수 있는 정보',
-                        result.data,
-                      );
+                      // console.log(
+                      //   item.name,
+                      //   '드라이버가 저장한 루트id를 통해 얻을 수 있는 정보',
+                      //   result.data,
+                      // );
                       // 루트 정보의 date값과 routeType을 비교하여(ex)date : 2022-01-01 routeType : lunch)
-                      // (id,userId, name, routeInfo)라는 속성을 가지는 새로운 객체배열에 저장
+                      // (id, userId, name, routeInfo)라는 속성을 가지는 새로운 객체배열에 저장
                       if (
+                        // true
                         // realDate === result.data.date &&
                         realType === result.data.routeType
                       ) {
                         setTempList(tempList => {
                           const newTempList = [...tempList];
                           newTempList.push({
-                            userId: item.email,
                             id: item.id,
+                            userId: item.email,
                             name: item.name,
                             routeInfo: result.data,
                           });
@@ -103,18 +100,21 @@ export default function ManagerGPS({navigation}) {
             })
             .catch(e => {
               console.log(e);
-            })
-            .finally(fin => {
-              setOk(true);
             });
         });
+      // setOk(!ok);
+      // console.log(newTemp);
+      // setTempList(newTemp);
     }
+  };
+
+  useEffect(() => {
+    setInfoList();
   }, [userList]);
 
   useEffect(() => {
     //모든 루트 정보가 저장되었을 시 실행
     if (tempList.length !== 0) {
-      console.log(tempList);
       tempList.map(item => {
         //routeInfo의 done이 false일때만 진행
         if (!item.routeInfo.done) {
@@ -151,8 +151,9 @@ export default function ManagerGPS({navigation}) {
           setPickupFinish(finishPickupSum);
           setDeliveryTotal(totalDeliverySum);
           setDeliveryFinish(finishDeliverySum);
-          const index = list.findIndex(find => {
-            find.routeName === item.routeInfo.routeName;
+
+          const index = list.findIndex(function (find) {
+            return find.routeName === item.routeInfo.routeName;
           });
           if (index === -1) {
             setDriverList(driverList => {
@@ -172,19 +173,9 @@ export default function ManagerGPS({navigation}) {
                 check = true;
               }
             });
-            //이미 존재하는 드라이버인 경우
-            if (check) {
-              setList(list => {
-                const newList = [...list];
-                newList[index].pickupTotal += totalPickupSum;
-                newList[index].pickupFinish += finishPickupSum;
-                newList[index].deliveryTotal += totalDeliverySum;
-                newList[index].deliveryFinish += finishDeliverySum;
-                return newList;
-              });
-            }
+
             //존재하지 않는 드라이버인 경우 드라이버 추가, 드라이버 숫자 업데이트
-            else {
+            if (!check) {
               setList(list => {
                 const newList = [...list];
                 newList[index].driver.push({
@@ -203,11 +194,10 @@ export default function ManagerGPS({navigation}) {
         }
       });
     }
-  }, [ok]);
+  }, [tempList]);
 
   useEffect(() => {
     //새로 추가되는 학교일 때 실행(list 배열에 새로운 객체 업데이트)
-    console.log('useEffec문 실행', index);
     if (index === -1) {
       setList(list => {
         const newList = [...list];
@@ -230,13 +220,6 @@ export default function ManagerGPS({navigation}) {
     }
   }, [index]);
 
-  useEffect(() => {
-    if (list.length !== 0) {
-      // console.log('list update');
-      // console.log('업데이트 worklist', list);
-    }
-  }, [list]);
-
   function leftPad(value) {
     if (value >= 10) {
       return value;
@@ -248,6 +231,7 @@ export default function ManagerGPS({navigation}) {
     const year = source.getFullYear();
     const month = leftPad(source.getMonth() + 1);
     const day = leftPad(source.getDate());
+
     return [year, month, day].join(delimiter);
   }
 
