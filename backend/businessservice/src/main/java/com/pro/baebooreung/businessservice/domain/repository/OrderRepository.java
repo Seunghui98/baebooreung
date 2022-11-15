1824ca4fb1803f3a06ff563000a3b1a5e9d30eba
LeeSeungHui <hithere1012@naver.com>
2022-11-15 14:15:59 +0900

    [B] ADD_Counting service
    비즈니스 로직 내에 주문건수 들어가는 서비스 추가

---
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/domain/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.pro.baebooreung.businessservice.domain.repository;
+
+import com.pro.baebooreung.businessservice.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<OrderRepository, Integer> {
+    List<Order> findByDelivery(int deliveryId);
+}
