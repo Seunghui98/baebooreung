98bd811a4513ca49b130dc738d01a4c82fad38f5
daseul <dew4325@naver.com>
2022-11-15 10:52:00 +0900

    [B]FIX_API

--- backend/s3service/src/main/java/com/pro/baebooreung/s3service/client/DeliveryTaskClient.java
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/client/DeliveryTaskClient.java
@@ -6,12 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "business-service")
 public interface DeliveryTaskClient {
 
     @PostMapping("/delivery/save/Img")
-    void saveImg(CheckResponse res);
+    void saveImg(@RequestBody CheckResponse res);
 
     @GetMapping("/delivery/Img/{delId}")
     String getImg(@PathVariable("delId") int delId);
