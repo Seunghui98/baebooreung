0217264c29726e676a1d589dc046b712deed5dbb
kaydenna92 <61959111+kaydennna92@users.noreply.github.com>
2022-11-15 22:30:26 +0900

    [F] UPDATE_MAPUI

--- frontend/app/src/pages/driverApp/DetaliRoutePage.js
+++ frontend/app/src/pages/driverApp/DetaliRoutePage.js
@@ -1,21 +1,10 @@
-import {React, useEffect, useState} from 'react';
+import {React} from 'react';
 import {Dimensions, StyleSheet, View} from 'react-native';
 import {SwiperFlatList} from 'react-native-swiper-flatlist';
 import DetailJob from '../../components/DetailJob';
-import {useDispatch, useSelector} from 'react-redux';
-import {setGps} from '../../redux/gps';
-import {getLocationPermission} from '../../utils/permission';
-import Geolocation from 'react-native-geolocation-service';
-import {useNavigation} from '@react-navigation/native';
 
 export const DetailRoutePage = props => {
   // 경로 데이터
-  const lunchDate = useSelector(state => state.work.lunchDate);
-  const dinnerDate = useSelector(state => state.work.dinnerDate);
-  const lunchRoute = useSelector(state => state.work.lunchRoute);
-  const lunchDone = useSelector(state => state.work.lunchDone);
-  const dinnerRoute = useSelector(state => state.work.dinnerRoute);
-  const dispatch = useDispatch();
   // const [currentLocation, setCurrentLocation] = useState(false);
   // // <------------------------current position-------------------------->
   // const getCurrentLocation = () => {
@@ -44,78 +33,78 @@ export const DetailRoutePage = props => {
   //   });
   // };
   // <------------------------kafka data set---------------------------->
-  const setKafka = () => {
-    const kafka = {
-      userId: 2,
-      latitude: watchLocation.latitude,
-      longitude: watchLocation.longitude,
-      requestDateTime: String(watchLocation.requestDateTime),
-    };
-    return kafka;
-  };
+  // const setKafka = () => {
+  //   const kafka = {
+  //     userId: 2,
+  //     latitude: watchLocation.latitude,
+  //     longitude: watchLocation.longitude,
+  //     requestDateTime: String(watchLocation.requestDateTime),
+  //   };
+  //   return kafka;
+  // };
 
-  useEffect(() => {
-    if (watchLocation !== false) {
-      sendGps(setKafka());
-    }
-  }, [watchLocation]);
+  // useEffect(() => {
+  //   if (watchLocation !== false) {
+  //     sendGps(setKafka());
+  //   }
+  // }, [watchLocation]);
 
-  // <-------------------------watch position-------------------------->
-  const watchId = null;
-  const [watchLocation, setWatchLocation] = useState(false);
-  const getWatchLocation = () => {
-    // console.log('getWatchLocation is running...');
-    const permission = getLocationPermission();
-    permission.then(granted => {
-      if (granted) {
-        this.watchId = Geolocation.watchPosition(
-          position => {
-            const {latitude, longitude} = position.coords;
-            const date = new Date(position.timestamp)
-              .toISOString()
-              .split('.')[0];
-            setWatchLocation({
-              latitude: latitude,
-              longitude,
-              longitude,
-              requestDateTime: date,
-            });
-            dispatch(
-              setGps({
-                lat: latitude,
-                lng: longitude,
-              }),
-            );
-          },
-          error => {
-            // console.log("driverApp/Gps => getWatchLocation's error", error);
-            setWatchLocation(false);
-          },
-          {enableHighAccuracy: true, fastestInterval: 3000, distanceFilter: 0},
-        );
-      }
-    });
-    // console.log(watchLocation);
-  };
+  // // <-------------------------watch position-------------------------->
+  // const watchId = null;
+  // const [watchLocation, setWatchLocation] = useState(false);
+  // const getWatchLocation = () => {
+  //   // console.log('getWatchLocation is running...');
+  //   const permission = getLocationPermission();
+  //   permission.then(granted => {
+  //     if (granted) {
+  //       this.watchId = Geolocation.watchPosition(
+  //         position => {
+  //           const {latitude, longitude} = position.coords;
+  //           const date = new Date(position.timestamp)
+  //             .toISOString()
+  //             .split('.')[0];
+  //           setWatchLocation({
+  //             latitude: latitude,
+  //             longitude,
+  //             longitude,
+  //             requestDateTime: date,
+  //           });
+  //           dispatch(
+  //             setGps({
+  //               lat: latitude,
+  //               lng: longitude,
+  //             }),
+  //           );
+  //         },
+  //         error => {
+  //           // console.log("driverApp/Gps => getWatchLocation's error", error);
+  //           setWatchLocation(false);
+  //         },
+  //         {enableHighAccuracy: true, fastestInterval: 3000, distanceFilter: 0},
+  //       );
+  //     }
+  //   });
+  //   // console.log(watchLocation);
+  // };
 
   // useEffect(() => {
   //   // 업무시작 버튼을 누르면 실행되게 하기.
   //   getWatchLocation();
   // }, []);
   // <-------------------------kill watch position-------------------------->
-  const killWatchLocation = () => {
-    if (this.watchId !== null) {
-      Geolocation.clearWatch(this.watchId);
-      setWatchLocation(false);
-      // console.log('getWatchLocation is stop...');
-    }
-  };
+  // const killWatchLocation = () => {
+  //   if (this.watchId !== null) {
+  //     Geolocation.clearWatch(this.watchId);
+  //     setWatchLocation(false);
+  //     // console.log('getWatchLocation is stop...');
+  //   }
+  // };
   const {width} = Dimensions.get('window');
 
   const renderItem = ({item}) => {
     return (
       <View style={{width: width}}>
-        <DetailJob item={item} date={lunchDate} />
+        <DetailJob item={item} />
       </View>
     );
   };
