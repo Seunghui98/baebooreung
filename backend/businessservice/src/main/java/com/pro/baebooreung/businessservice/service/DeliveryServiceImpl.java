c72d176a901a19f79930586a3583c96224a94997
daseul <dew4325@naver.com>
2022-11-15 11:15:08 +0900

    [B]FIX_API

--- backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/service/DeliveryServiceImpl.java
+++ backend/businessservice/src/main/java/com/pro/baebooreung/businessservice/service/DeliveryServiceImpl.java
@@ -30,7 +30,7 @@ public class DeliveryServiceImpl implements DeliveryService {
 
     @Override
     public void saveImg(CheckResponse res) {
-//        log.info(res.getDelId()+":delivery Id");
+        log.info(res.getDelId()+":delivery Id!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         Optional<Delivery> findDel = deliveryRepository.findById(res.getDelId());
         Delivery delivery = findDel.get();
 //        log.info(delivery+":delivery!!!!!!!!!");
