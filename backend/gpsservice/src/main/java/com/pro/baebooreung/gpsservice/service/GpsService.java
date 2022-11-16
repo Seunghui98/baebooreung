6476f85be62ee8d665a0eac21de92f5ece234a18
이승희 <hithere1012@naver.com>
2022-11-17 00:07:06 +0900

    [B] FIX_gps service
    gps service 내에 루트, 유저기반으로 gpslist get하는 api 기능 추가

--- backend/gpsservice/src/main/java/com/pro/baebooreung/gpsservice/service/GpsService.java
+++ backend/gpsservice/src/main/java/com/pro/baebooreung/gpsservice/service/GpsService.java
@@ -7,6 +7,7 @@ import com.pro.baebooreung.gpsservice.domain.repository.UserRepository;
 import com.pro.baebooreung.gpsservice.dto.GpsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +26,43 @@ public class GpsService {
                 .longitude(String.valueOf(gps.getLongitude()))
                 .requestDateTime(gps.getRequestDateTime()).build();
         return gpsResponseDto;
+    }
+
+    public List<GpsResponseDto> findGpsByRouteId(int routeId) throws Exception {
+        List<Gps> gps = gpsRepository.findByRouteId(routeId);
+        List<GpsResponseDto> gpsList = new ArrayList<>();
+        if(gps == null || gps.size() == 0){
+            return gpsList;
+
+        }
+        for(Gps g:gps){
+            GpsResponseDto gpsResponseDto = GpsResponseDto.builder()
+                    .latitude(String.valueOf(g.getLatitude()))
+                    .longitude(String.valueOf(g.getLongitude()))
+                    .requestDateTime(g.getRequestDateTime()).build();
+            gpsList.add(gpsResponseDto);
+        }
+        return gpsList;
+    }
+
+    public List<GpsResponseDto> findGpsByUserId(int userId) throws Exception {
+        User user = userRepository.findOne(userId);
+        if(user.getWorkStatus() == null || user.getWorkStatus().equals("OFF")){
+            throw new IllegalStateException("현재 운행중인 드라이버가 아닙니다.");
+        }
+        List<Gps> gps = gpsRepository.findByUserId(userId);
+        List<GpsResponseDto> gpsList = new ArrayList<>();
+        if(gps == null || gps.size() == 0){
+            return gpsList;
+
+        }
+        for(Gps g:gps){
+            GpsResponseDto gpsResponseDto = GpsResponseDto.builder()
+                    .latitude(String.valueOf(g.getLatitude()))
+                    .longitude(String.valueOf(g.getLongitude()))
+                    .requestDateTime(g.getRequestDateTime()).build();
+            gpsList.add(gpsResponseDto);
+        }
+        return gpsList;
     }
 }
