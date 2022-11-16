dacff670d740bbff30ad42ab71627df737f8e12a
daseul <dew4325@naver.com>
2022-11-16 09:33:21 +0900

    [B]ADD_Name

--- backend/chatservice/src/main/java/com/pro/baebooreung/chat/dto/ChatMessage.java
+++ backend/chatservice/src/main/java/com/pro/baebooreung/chat/dto/ChatMessage.java
@@ -19,12 +19,13 @@ public class ChatMessage {
     }
 
     @Builder
-    public ChatMessage(MessageType type, String roomId, String sender, String message, long userCount) {
+    public ChatMessage(MessageType type, String roomId, String sender, String message, long userCount, String name) {
         this.type = type;
         this.roomId = roomId;
         this.sender = sender;
         this.message = message;
         this.userCount = userCount;
+        this.name = name;
     }
     public enum MessageType{
         ENTER, TALK, QUIT
@@ -34,4 +35,5 @@ public class ChatMessage {
     private String sender; //메시지 보낸 사람
     private String message; // 메시지
     private long userCount; //채팅방 인원 수, 채팅방 내에서 메시지가 전달될때 인원수 갱신시 사용
+    private String name;
 }
