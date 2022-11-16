86029cb7364ebf012e61a2d1fb60ae0ef27fa8ca
LeeJieuni <lgeun123@naver.com>
2022-11-17 01:31:16 +0900

    [B]FIX_apigateway

--- backend/apigatewayservice/src/main/java/com/pro/baebooreung/apigatewayservice/config/WebMvcConfig.java
+++ backend/apigatewayservice/src/main/java/com/pro/baebooreung/apigatewayservice/config/WebMvcConfig.java
@@ -5,17 +5,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name());
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods(
+//                        HttpMethod.GET.name(),
+//                        HttpMethod.HEAD.name(),
+//                        HttpMethod.POST.name(),
+//                        HttpMethod.PUT.name(),
+//                        HttpMethod.DELETE.name());
+//    }
 }
