4ee473796f2d3c5fc4dd554d9f1dd0ec2a686bff
이승희 <hithere1012@naver.com>
2022-11-16 22:48:56 +0900

    [B] FIX_userdto
    user service내에 userdto에 phone, workstatus 추가

--- backend/userservice/src/main/java/com/pro/baebooreung/userservice/vo/ResponseUser.java
+++ backend/userservice/src/main/java/com/pro/baebooreung/userservice/vo/ResponseUser.java
@@ -1,5 +1,6 @@
 package com.pro.baebooreung.userservice.vo;
 
+import com.pro.baebooreung.userservice.domain.WorkStatus;
 import lombok.Data;
 
 import java.util.List;
@@ -11,5 +12,7 @@ public class ResponseUser {
     private String name;
     private String specialKey;
     private String grade;
+    private String phone;
+    private WorkStatus workStatus;
     private List<ResponseRoute> routeList;
 }
