ee121332f13ff9a8ed39a8eed8b0405017cc230e
LeeJieuni <lgeun123@naver.com>
2022-11-17 01:34:36 +0900

    [B]FIX_apigateway

--- backend/apigatewayservice/src/main/java/com/pro/baebooreung/apigatewayservice/config/WebMvcConfig.java
+++ backend/apigatewayservice/src/main/java/com/pro/baebooreung/apigatewayservice/config/WebMvcConfig.java
@@ -1,9 +1,9 @@
 package com.pro.baebooreung.apigatewayservice.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
 public class WebMvcConfig{// implements WebMvcConfigurer {
