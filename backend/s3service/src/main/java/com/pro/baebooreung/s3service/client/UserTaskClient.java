4d290c196ec394da3eb2092b5fb20d52ef3ecf2a
daseul <dew4325@naver.com>
2022-11-16 11:13:15 +0900

    [B]FIX_SAVE api

--- backend/s3service/src/main/java/com/pro/baebooreung/s3service/client/UserTaskClient.java
+++ backend/s3service/src/main/java/com/pro/baebooreung/s3service/client/UserTaskClient.java
@@ -5,12 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "user-service")
 public interface UserTaskClient {
 
     @PostMapping("/user/save/profile")
-    void saveProfile(ProfileResponse res);
+    void saveProfile(@RequestBody ProfileResponse res);
 
     @GetMapping("/user/profile/{userId}")
     String getProfile(@PathVariable("userId") int userId);
